b130fe592f3dc112e952551cd7bd3232865ae082
vmoseng <vmoseng@outlook.com>
2017-04-08 06:57:53 -0700

    Custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :portfolios
 
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
   
